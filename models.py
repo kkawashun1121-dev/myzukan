cf118b24351dd5917f59748a593e173096f9c0af
kkawashun1121-dev <kkawashun1121@gmail.com>
2026-06-15 18:41:15 +0900

    データベース設定

---
+++ models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Boolean,ForeignKey
+from database import Base
+
+class Creature(Base):
+    __tablename__="zukan"
+
+    id=Column(Integer, primary_key=True,index=True)
+    owner_id=Column(Integer,ForeignKey("users.id"))
+    name=Column(String,nullable=False)
+    image_url=Column(String,nullable=True)
+    care_guide=Column(String,nullable=True)
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer,primary_key=True,index=True)
+    username=Column(String,unique=True,nullable=False)
+    hashed_password=Column(String,nullable=False)
