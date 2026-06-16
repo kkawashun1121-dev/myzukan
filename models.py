37b86991779eae8ef2aa8c3f4047dc2a0bf3c040
kkawashun1121-dev <kkawashun1121@gmail.com>
2026-06-16 15:26:32 +0900

    cageクラス追加

--- models.py
+++ models.py
@@ -10,9 +10,20 @@ class Creature(Base):
     image_url=Column(String,nullable=True)
     care_guide=Column(String,nullable=True)
 
+
+
+
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer,primary_key=True,index=True)
     username=Column(String,unique=True,nullable=False)
     hashed_password=Column(String,nullable=False)
+
+class Cage(Base):
+    __tablename__="cages"
+
+    id=Column(Integer,primary_key=True,index=True)
+    owner_id=Column(Integer,ForeignKey("users.id"))
+    name=Column(String,nullable=False)
+    
