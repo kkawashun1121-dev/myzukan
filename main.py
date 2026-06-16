ef0cf567d379dd64144f3df3dda13a5b243bf65b
kkawashun1121-dev <kkawashun1121@gmail.com>
2026-06-16 15:44:57 +0900

    cage crud　作成

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
-from models import Creature, User, Base
+from models import Creature, User,Cage,Base
 
 from fastapi.security import OAuth2PasswordRequestForm
 from auth import hash_password, verify_password, create_access_token,get_current_user
@@ -27,51 +27,8 @@ class UserCreate(BaseModel):
     username: str
     password: str
 
-def get_current_db_user(db: Session = Depends(get_db), current_user: str = Depends(get_current_user)):
-    db_user = db.query(User).filter(User.username==current_user).first()
-    return db_user
-
-@app.post('/creatures')
-def create_creature(zukan:CreatureCreate,
-                    db:Session = Depends(get_db),
-                    db_user=Depends(get_current_db_user)
-                    ):
-    new_creature=Creature(
-        name=zukan.name,
-        image_url=zukan.image_url,
-        care_guide=zukan.care_guide,
-        owner_id = db_user.id)
-    
-    db.add(new_creature)
-    db.commit()
-    db.refresh(new_creature)
-    return new_creature
-
-@app.put("/creatures/{creature_id}")
-def update_creature(creature_id:int,
-                    zukan:CreatureCreate,
-                    db:Session=Depends(get_db),
-                    db_user=Depends(get_current_db_user)
-                    ):
-    creature=db.query(Creature).filter(Creature.id==creature_id,Creature.owner_id==db_user.id).first()
-    if not creature:
-        raise HTTPException(status_code=404,detail=f"生物{creature_id}が見つかりません")
-    creature.name=zukan.name
-    creature.image_url=zukan.image_url
-    creature.care_guide=zukan.care_guide
-    db.commit()
-    db.refresh(creature)
-    return creature
-
-@app.delete("/creatures/{creature_id}")
-def delete_creature(creature_id:int,db:Session=Depends(get_db),db_user=Depends(get_current_db_user)):
-    
-    creature=db.query(Creature).filter(Creature.id==creature_id,Creature.owner_id==db_user.id).first()
-    if not creature:
-        raise HTTPException(status_code=404,detail=f"生物{creature_id}が見つかりません")
-    db.delete(creature)
-    db.commit()
-    return f'{creature_id}が削除されました。'
+class CageCreate(BaseModel):
+    name:str
 
 @app.post('/register')
 def register(user:UserCreate,db:Session= Depends(get_db)):
@@ -96,3 +53,102 @@ def login(form_data:OAuth2PasswordRequestForm=Depends(),db:Session=Depends(get_d
     
     token=create_access_token({"sub":db_user.username})
     return {"access_token":token,"token_type":"bearer"}
+
+
+def get_current_db_user(db: Session = Depends(get_db), current_user: str = Depends(get_current_user)):
+    db_user = db.query(User).filter(User.username==current_user).first()
+    return db_user
+
+
+@app.post('/creatures')
+def create_creature(zukan:CreatureCreate,
+                    db:Session = Depends(get_db),
+                    db_user=Depends(get_current_db_user)
+                    ):
+    new_creature=Creature(
+        name=zukan.name,
+        image_url=zukan.image_url,
+        care_guide=zukan.care_guide,
+        owner_id = db_user.id)
+    
+    db.add(new_creature)
+    db.commit()
+    db.refresh(new_creature)
+    return new_creature
+
+@app.get("/creatures")
+def get_creatures(db: Session = Depends(get_db), db_user=Depends(get_current_db_user)):
+    return db.query(Creature).filter(Creature.owner_id == db_user.id).all()
+
+@app.put("/creatures/{creature_id}")
+def update_creature(creature_id:int,
+                    zukan:CreatureCreate,
+                    db:Session=Depends(get_db),
+                    db_user=Depends(get_current_db_user)
+                    ):
+    creature=db.query(Creature).filter(Creature.id==creature_id,Creature.owner_id==db_user.id).first()
+    if not creature:
+        raise HTTPException(status_code=404,detail=f"生物{creature_id}が見つかりません")
+    creature.name=zukan.name
+    creature.image_url=zukan.image_url
+    creature.care_guide=zukan.care_guide
+    db.commit()
+    db.refresh(creature)
+    return creature
+
+@app.delete("/creatures/{creature_id}")
+def delete_creature(creature_id:int,db:Session=Depends(get_db),db_user=Depends(get_current_db_user)):
+    creature=db.query(Creature).filter(Creature.id==creature_id,Creature.owner_id==db_user.id).first()
+    if not creature:
+        raise HTTPException(status_code=404,detail=f"生物{creature_id}が見つかりません")
+    db.delete(creature)
+    db.commit()
+    return f'{creature_id}が削除されました。'
+
+
+
+@app.post("/cages")
+def post_cage(cage:CageCreate,
+                    db:Session = Depends(get_db),
+                    db_user=Depends(get_current_db_user)
+                    ):
+    new_cage=Cage(name=cage.name,owner_id=db_user.id)
+    
+    db.add(new_cage)
+    db.commit()
+    db.refresh(new_cage)
+    return new_cage
+
+@app.get("/cages")
+def get_cages(db:Session=Depends(get_db),db_user=Depends(get_current_db_user)):
+    return db.query(Cage).filter(Cage.owner_id==db_user.id).all()
+
+@app.get("/creatures")
+def get_creatures(db: Session = Depends(get_db), db_user=Depends(get_current_db_user)):
+    return db.query(Creature).filter(Creature.owner_id == db_user.id).all()
+
+
+@app.put("/cages/{cage_id}")
+def update_creature(cage_id:int,
+                    cage:CageCreate,
+                    db:Session=Depends(get_db),
+                    db_user=Depends(get_current_db_user)
+                    ):
+    cage_name=db.query(Cage).filter(Cage.id==cage_id,Cage.owner_id==db_user.id).first()
+    if not cage_name:
+        raise HTTPException(status_code=404,detail=f"カゴ{cage_id}が見つかりません")
+    cage_name.name=cage.name
+
+    db.commit()
+    db.refresh(cage_name)
+    return cage_name
+
+@app.delete("/cages/{cage_id}")
+def delete_cage(cage_id:int,db:Session=Depends(get_db),db_user=Depends(get_current_db_user)):
+    cage_name=db.query(Cage).filter(Cage.id==cage_id,Cage.owner_id==db_user.id).first()
+    if not cage_name:
+        raise HTTPException(status_code=404,detail=f"カゴ{cage_id}が見つかりません")
+    db.delete(cage_name)
+    db.commit()
+    return f'{cage_id}が削除されました。'
+
